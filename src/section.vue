55500dab34bc0497179053932b818010a12b137e
fbriou <fbriou@gmail.com>
2019-06-12 14:06:34 +0200

    start section

--- src/section.vue
+++ src/section.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="feature_C section-side-padding">
+    <div class="blog-horizontal-cards">
         <!-- wwManager:start -->
         <wwSectionEditMenu :sectionCtrl="sectionCtrl" :options="openOptions"></wwSectionEditMenu>
         <!-- wwManager:end -->
@@ -13,19 +13,22 @@
         </div>
 
         <!--THUMBNAILS-->
-        <div class="container">
-            <div class="container-center">
-                <div class="thumbnail-container" v-for="(thumbnail, index) in section.data.thumbnails" :key="thumbnail.uniqueId" :style="columnWidth">
-                    <!-- wwManager:start -->
-                    <wwContextMenu tag="div" class="contextmenu" v-if="sectionCtrl.getEditMode() == 'CONTENT'" @ww-add-before="addCardBefore(index)" @ww-add-after="addCardAfter(index)" @ww-remove="removeCard(index)">
-                        <div class="wwi wwi-config"></div>
-                    </wwContextMenu>
-                    <!-- wwManager:end -->
-                    <wwObject class="background" :ww-object="thumbnail.background" ww-category="background" ww-default-object-type="ww-color"></wwObject>
+        <div class="thumbnail-container">
+            <div class="thumbnail" v-for="(thumbnail, index) in section.data.thumbnails" :key="thumbnail.uniqueId">
+                <!-- wwManager:start -->
+                <wwContextMenu tag="div" class="contextmenu" v-if="sectionCtrl.getEditMode() == 'CONTENT'" @ww-add-before="addCardBefore(index)" @ww-add-after="addCardAfter(index)" @ww-remove="removeCard(index)">
+                    <div class="wwi wwi-config"></div>
+                </wwContextMenu>
+                <!-- wwManager:end -->
 
+                <div class="content-container" :style="{'order': isPair(index)}">
+                    <wwObject class="background" :ww-object="thumbnail.background" ww-category="background" ww-default-object-type="ww-color"></wwObject>
                     <wwLayoutColumn tag="div" ww-default="ww-image" :ww-list="thumbnail.contents" class="content" @ww-add="add(thumbnail.contents, $event)" @ww-remove="remove(thumbnail.contents, $event)">
                         <wwObject v-for="content in thumbnail.contents" :key="content.uniqueId" :ww-object="content"></wwObject>
                     </wwLayoutColumn>
+                </div>
+                <div class="image-container">
+                    <wwObject class="background" :ww-object="thumbnail.image" ww-category="background" ww-default-object-type="ww-image" :style="{'border-top-right-radius': '7px', 'border-bottom-right-radius': '7px'}"></wwObject>
                 </div>
             </div>
         </div>
@@ -70,7 +73,6 @@ export default {
     data() {
         return {
             maxThumbnailsPerLine: 4,
-            columnWidth: { 'width': "calc(25% - 30px)" }
         }
     },
     computed: {
@@ -102,6 +104,7 @@ export default {
             if (_.isEmpty(this.section.data.thumbnails)) {
                 this.section.data.thumbnails.push({
                     background: wwLib.wwObject.getDefault({ type: 'ww-color', data: { color: 'white' } }),
+                    image: wwLib.wwObject.getDefault({ type: 'ww-image', data: {} }),
                     contents: []
                 })
                 needUpdate = true;
@@ -115,9 +118,17 @@ export default {
             }
         },
         init() {
-            this.setThumbnailsPerLine();
             window.addEventListener("resize", this.setThumbnailsPerLine);
         },
+        isPair(index) {
+            if (index % 2) {
+                return 1;
+            } else {
+                return 0;
+            }
+        },
+
+        /* wwManager:start */
         add(list, options) {
             list.splice(options.index, 0, options.wwObject);
 
@@ -128,60 +139,27 @@ export default {
 
             this.sectionCtrl.update(this.section);
         },
-        setThumbnailsPerLine() {
-            let width = window.innerWidth;
-            if (width < 576) {
-                this.maxThumbnailsPerLine = 1;
-            }
-            else if (width < 992) {
-                this.maxThumbnailsPerLine = 2;
-            }
-            else if (width < 1200) {
-                this.maxThumbnailsPerLine = 3;
-            }
-            else {
-                this.maxThumbnailsPerLine = 4;
-            }
-
-            switch (Math.min(this.section.data.thumbnailsPerLine, this.maxThumbnailsPerLine)) {
-                case 1:
-                    this.columnWidth = { 'width': "calc(100% - 30px)" };
-                    break;
-                case 2:
-                    this.columnWidth = { 'width': "calc(50% - 30px)" };
-                    break;
-                case 3:
-                    this.columnWidth = { 'width': "calc(33.3333% - 30px)" };
-                    break;
-                default:
-                    this.columnWidth = { 'width': "calc(25% - 30px)" };
-                    break;
-            }
+        getNewCard() {
+            const card = JSON.parse(JSON.stringify(this.section.data.thumbnails[0]))
+            wwLib.wwUtils.changeUniqueIds(card)
+            return card
         },
-
-        /* wwManager:start */
         addCardBefore(index) {
-            const newCard = {
-                background: wwLib.wwObject.getDefault({ type: 'ww-color', data: { color: 'white' } }),
-                contents: []
-            }
+            const newCard = this.getNewCard()
             this.section.data.thumbnails.splice(index, 0, newCard);
             this.sectionCtrl.update(this.section);
         },
         addCardAfter(index) {
-            const newCard = {
-                background: wwLib.wwObject.getDefault({ type: 'ww-color', data: { color: 'white' } }),
-                contents: []
-            }
+            const newCard = this.getNewCard()
             this.section.data.thumbnails.splice(index + 1, 0, newCard);
             this.sectionCtrl.update(this.section);
         },
         removeCard(index) {
+            if (!this.section.data.thumbnails.length) {
+                return;
+            }
             this.section.data.thumbnails.splice(index, 1);
 
-            if (!this.section.data.thumbnails.length) {
-                this.addCardAfter(0);
-            }
             this.sectionCtrl.update(this.section);
         },
 
@@ -222,7 +200,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.feature_C {
+.blog-horizontal-cards {
     position: relative;
 
     .background {
@@ -243,64 +221,76 @@ export default {
         }
     }
 
-    .container {
-        .container-center {
+    .thumbnail-container {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+
+        .thumbnail {
             display: flex;
-            justify-content: center;
-            flex-wrap: wrap;
+            width: 75%;
+            margin-right: 15px;
+            position: relative;
+            margin: 30px 15px;
+            background-color: white;
+            min-height: 50px;
+            box-shadow: 0 10px 40px 0 rgba(113, 124, 137, 0.2);
+            border-radius: 7px;
+            transition: transform 0.4s ease-out, box-shadow 0.4s ease-out;
 
-            .thumbnail-container {
-                margin-right: 15px;
+            &:hover {
+                box-shadow: 0 16px 32px rgba(113, 124, 137, 0.4);
+                transform: translateY(-10px);
+            }
+
+            .content {
                 position: relative;
-                margin: 30px 15px;
-                background-color: white;
-                min-height: 50px;
-                box-shadow: 0 10px 40px 0 rgba(113, 124, 137, 0.2);
-                border-radius: 7px;
-                transition: transform 0.4s ease-out, box-shadow 0.4s ease-out;
+            }
 
-                &:hover {
-                    box-shadow: 0 16px 32px rgba(113, 124, 137, 0.4);
-                    transform: translateY(-10px);
-                }
+            .content-container {
+                position: relative;
+                width: 50%;
+            }
 
+            .image-container {
+                position: relative;
+                width: 50%;
                 .background {
-                    border-radius: 7px;
                     overflow: hidden;
                 }
-
-                .content {
-                    position: relative;
-                }
-
-                /* wwManager:start */
-                .contextmenu {
-                    position: absolute;
-                    top: 0;
-                    left: 0;
-                    transform: translate(-50%, -50%);
-                    width: 30px;
-                    height: 30px;
-                    color: white;
-                    background-color: #ef811a;
-                    border-radius: 100%;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    font-size: 1.2rem;
-                    cursor: pointer;
-                    z-index: 1;
-                }
-                /* wwManager:end */
             }
+
+            /* wwManager:start */
+            .contextmenu {
+                position: absolute;
+                top: 0;
+                left: 0;
+                transform: translate(-50%, -50%);
+                width: 30px;
+                height: 30px;
+                color: white;
+                background-color: #ef811a;
+                border-radius: 100%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                font-size: 1.2rem;
+                cursor: pointer;
+                z-index: 1;
+            }
+            /* wwManager:end */
         }
     }
 }
 
 /* wwManager:start */
-.ww-editing .thumbnail-container {
-    &:hover {
-        transform: none !important;
+.ww-editing {
+    .thumbnail-container {
+        .thumbnail {
+            &:hover {
+                transform: none !important;
+            }
+        }
     }
 }
 /* wwManager:end */
